56dc67a4bbb0845ccfe15c35d60962f893d44ef9
MohdAmaan1802 <mohdamaan1802@gmail.com>
2024-05-09 17:06:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import SplitPane from "react-split-pane";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SplitPane split="horizontal" minSize={50} defaultSize="50%">
+        <SplitPane split="vertical" minSize={100} defaultSize="50%">
+          <div className="pane-content">
+            <h1>Window-1</h1>
+            <p>Component-1 (some HTML content)</p>
+          </div>
+          <div className="pane-content">
+            <h1>Window-2</h1>
+            <p>Component-2 (some HTML content)</p>
+          </div>
+        </SplitPane>
+        <div className="pane-content">
+          <h1>Window-3</h1>
+          <p>Component-3 (some HTML content)</p>
+        </div>
+      </SplitPane>
     </div>
   );
 }
